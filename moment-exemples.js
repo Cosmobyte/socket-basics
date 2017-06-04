7b4dc9e15f3e71ac0c857779fd6b7961bb9ce574
Cosmin-Mirel Budeanu <cosmin.mirel18@gmail.com>
2017-06-04 15:34:58 +0200

    Add moment example and file

---
+++ moment-exemples.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+var now = moment();
+
+console.log(now.format());
+console.log(now.format('X'));
+console.log(now.valueOf())
+
+var timestamp = 1496576417047;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('hh:mm a'));
+// now.subtract(1,'year');
+// console.log(now.format());
+// console.log(now.format('MMM Do YYYY, h:mm a'));
