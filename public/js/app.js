c29f5d8764d5bb68a14322484136c439a0b6b07c
Cosmin-Mirel Budeanu <cosmin.mirel18@gmail.com>
2017-06-04 17:19:13 +0200

    Show timestamp to UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,28 @@
 var socket = io();
 
-socket.on('connect',function(){
-	console.log("Connected to the socket.io server!");
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
 });
 
-socket.on('message',function(message){
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p>'+ message.text +'</p>');
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm:ss a') + ': </strong>' + message.text + '</p>');
 });
 
-//Handles submitting of new messages
+// Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit',function(event){
+$form.on('submit', function (event) {
 	event.preventDefault();
 
 	var $message = $form.find('input[name=message]');
-	socket.emit('message',{
+
+	socket.emit('message', {
 		text: $message.val()
 	});
+
 	$message.val('');
 });
