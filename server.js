88935c1e2b678bb5d36bdcfe372995aa1d09fe9d
Cosmin-Mirel Budeanu <cosmin.mirel18@gmail.com>
2017-06-05 12:01:58 +0200

    Add disconnect event and message

--- server.js
+++ server.js
@@ -23,12 +23,25 @@ io.on('connection', function (socket) {
 		});
 	});
 
-	socket.on('message', function (message) {
+	
+		socket.on('disconnect',function(){
+			var userData = clientInfo[socket.id];
+			if(typeof userData !== 'undefined'){
+				socket.leave(userData.room);
+				io.to(userData.room).emit('message',{
+					name: 'System',
+					text: userData.name + ' has left!',
+					timestamp : moment.valueOf()
+				});
+				delete clientInfo[socket.id];
+			}
+		});
+		socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
 		message.timestamp = moment().valueOf();
 		io.to(clientInfo[socket.id].room).emit('message', message);
-	});
+	    });
 
 	// timestamp property - JavaScript timestamp (milliseconds)
 
